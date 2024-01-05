640158767dbaec739d6658e19176a2a993818ab6
poorinut kaithong <s6603052413094@email.kmutnb.ac.th>
2024-01-05 09:15:31 +0700

    Exe in php
    Exe in php

--- a.php
+++ a.php
@@ -6,7 +6,20 @@
     <title>Document</title>
 </head>
 <body>
-    <?php  echo "Hello World"; ?>
+    <?php
+    $ages=array('Bob'=>20,'Peter'=>35);
+
+    echo "1 - ".$ages['Bob']."<br>";
+    echo "2 - ".$ages['Peter']."<br>";
+
+
+    $ages['Lek']=18;
+    $ages['Ying']=42;
+
+    echo "3  - ".($ages['Lek']+$ages['Peter'])."(br)";
+    $ages['Peter'] = $ages['Bob'] - $ages['Peter']) . "<br>";
+    echo"4 -".$ages['Peter']."<br>";
+    ?>
 </body>
 </html>
   
