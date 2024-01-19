be511250a6aff782459ea5952f950a59a0fbcfe6
Poorinut Kaithong <s6603052413094@email.kmutnb.ac.th>
2024-01-19 21:41:31 +0700

    work6
    work6

--- newpost.php
+++ newpost.php
@@ -5,38 +5,35 @@ if(!isset($_SESSION['id'])){
     die();
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>newpost</title>
 </head>
 <body>
-    <h1 style="text-align: center;">Webboard-onii</h1>
+<h1 style="text-align: center;">Webboard-onii</h1>
     <hr>
-</body>
-<form>
-<table>
-<?php if(!isset($_SESSION["id"])) {
-    header("Location:index.php");
-    die();
-    }
-?>
-<table>
-    <tr><td>ผู้ใช้ :</td><td><?php echo $_SESSION['username']; ?></td></tr>
-    <tr><td>หมวดหมู่ :</td><td>
-        <select name="category">
-            <option value="genneral">เรื่องทั่วไป</option>
-            <option value="study">เรื่องเรียน</option>
-        </select> 
-    </td></tr>
-    <table style="border:0px">
-    <tr><td>หัวข้อ :</td><td><input type="text" name="topic" size="30"></td></tr>
-    <tr><td>เนื้อหา :</td><td><textarea name="contact"cols="30" rows="2"></textarea></td></tr>
-    <tr><td></td><td><input type="submit" value="บันทึกข้อความ"></td></tr>
-</table>
-</form>
+    <form action="">
+        <table>
+            <tr><td>ผู้ใช้:</td><td><?php echo$_SESSION['username'] ?></td></tr>
+            <tr><td>หมวดหมู่</td><td>
+                <select name="category">
+            <option value="general">--เรื่องทั่วไป--</option>
+              <option value="study">--เรื่องเรียน--</option>
+              </select>
+            </td></tr>
+            <tr><td>หัวข้อ:</td><td>
+            <input type="text" name="text" size="20">
+            </td></tr>
+            <tr><td>เนื้อหา:</td><td><textarea name='story'  row="10" cols="30">
+           
+        </textarea></td></tr>
+            <tr><td></td><td><input type="submit" value="บันทึกข้อความ"></td></tr>
+        </table>
+    </form>
+    
 </body>
 </html>
-
