5504684d37373a771648818cbc542ac3db1d40a7
poorinut kaithong <s6603052413094@email.kmutnb.ac.th>
2024-03-08 11:48:19 +0700

    08/03/2567
    08/03/2567

--- post.php
+++ post.php
@@ -3,39 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <title>verify_post</title>
 </head>
 <body>
-    <h1 style="text-align: center;">Webboard-onii</h1>
-    <hr>
-    <div style="text-align: center;">
-         ต้องการดูกระทู้หมายเลข <?php echo $_GET['id'] ?> <br>
-         <?php
-            $n = $_GET['id'];
-
-            if(($n%2)==0){
-                echo "กระทู้หมายเลขคู่";
-            } else {
-                echo "กระทู้หมายเลขคี่";
-            }
-
-         ?>
+<div class="containar-lg">
+    <h1 style="text-align: center;" class="mt-3">Webboard-onii</h1>
+    <?php include "nav.php" ?>
+    <div class="row mt-4">
+        <div class="col-lg-3 col-md-2 col-sm-1 "></div>
+        <div class="col-lg-6 col-md-8 col-sm-10 ">
+            <div class="card-header border-success mt-3">
+            <div class="card-header bg-primary txt-white">เเสดงความคิดเห็น</div>
+            <div class="card-body">
+                <form action="post_save.php method="post">
+                    <input type="hidden" name="post_id"
+                    value="<?= $_GET['id'];?>">
+                    <div class="row mb-3 justify-content-center">
+                        <div class="col-lg-10">
+                            <textarea name="comment" rows="8"
+                            class="form-control"required></textarea>
+                        </div>
+                    </div>
+            <div class="row">
+                <div class="col-lg-12 d-flex justify-content-center">
+                        <button type="submit" class="btn btn-success btn-sm text-white">
+                            <i class="bi bi-box-arrow-up-right"</i> ส่งข้อความ
+                        </button>
+                        <button type="reset" class="btn btn-danger btn-sm ms-2">
+                            <i class"bi bi-x-square></i> ยกเลิก</button>
+                </div>
+            </div>
+        </form>
+     </div>
     </div>
-    <br>
-    <table style="border: 2px solid black; width:40%;" align="center">
-        <tr>
-            <td style="background-color: #6cd2fe;">แสดงความคิดเห็น</td>
-        </tr>
-        <tr>
-            <td align="center">
-                <textarea cols="50" rows="15"></textarea>
-                <input type="submit" value="ส่งข้อความ">
-            </td>
-        </tr>
-    </table>
-    <br>
-    <div style="text-align: center;"> 
-        <a href="index.php">กลับไปหน้าหลัก</a>
     </div>
-</body>
-</html>
+    <div class="coi-lg-3 col-md-2 col-sm-1"></div>
+</div> 
+</div>
--- register_save.php
+++ register_save.php
@@ -8,7 +8,7 @@ if (isset($_POST['login'])){
     $gender=$_POST['gender'];
     $email=$_POST['email'];
 
-    $conn = new PDO("mysql:host=localhost;dbname=app66;charset=utf8", "root", "");
+    $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8", "root", "");
         
             $sql ="INSERT INTO user (login, password, name, gender,email,role)
             VALUES ('$login','$passwd','$name','$gender','$email','m')";
