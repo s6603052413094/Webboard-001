88da2af030e94ece6a562d060912160b57aac7b0
Poorinut Kaithong <s6603052413094@email.kmutnb.ac.th>
2024-04-05 19:41:52 +0700

    Work8
    Work8

--- register.php
+++ register.php
@@ -6,24 +6,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <title>Webboard-onii</title>
-
-    
+    <title>Register</title>
 </head>
 <body>
-    <script>
-        function OnBlurPwd(){
-            let pwd=document.getElementById("pwd");
-            let pwd2=document.getElementById("pwd2");
-            if(pwd.value !== pwd2.value){
-                alert("รหัสผ่านสองช่องไม่ตรงกัน");
-                pwd2.value = ""; 
-            }
-        }    
-    </script>
     <div class="container">
         <header>
-            <h1 style="text-align: center;" class="mt-3">REGISTER MEMBERS</h1>
+            <h1 style="text-align: center;" class="mt-3">Webboard-onii</h1>
         </header>
 
         <?php include "nav.php" ?>
@@ -32,8 +20,6 @@
             <div class="col-lg-3 col-md-2 col-sm-1"></div>
             <div class="col-lg-6 col-md-8 col-sm-10">
                 <?php
-                    session_start();
-                    
                     if (isset($_SESSION['add_login'])) {
                         if ($_SESSION['add_login']=="error") {
                             echo "<div class='alert alert-danger'> Duplicate account name or Datebase problem </div>";
@@ -44,52 +30,46 @@
                     }
                 ?>
                 <div class="card border-primary">
-                    <div class="card-header bg-primary text-white">Register</div>
+                    <div class="card-header bg-primary text-white">สมัครสมาชิก</div>
                     <div class="card-body">
                         <form action="register_save.php" method="POST">
                             <div class="row">
-                                <label class="col-lg-3 col-form-label">Username: </label>
+                                <label class="col-lg-3 col-form-label">ชื่อบัญชี: </label>
                                 <div class="col-lg-9">
                                     <input type="text" name="login" class="form-control" require>
                                 </div>
                             </div>
                             <div class="row mt-3">
-                                <label class="col-lg-3 col-form-label">Password: </label>
+                                <label class="col-lg-3 col-form-label">รหัสผ่าน: </label>
                                 <div class="col-lg-9">
-                                    <input type="password" name="pwd" id="pwd" class="form-control" require>
+                                    <input type="password" name="pwd" class="form-control" require>
                                 </div>
                             </div>
                             <div class="row mt-3">
-                                <label class="col-lg-3 col-form-label">Confirm Password: </label>
-                                <div class="col-lg-9">
-                                    <input type="password" name="pwd" id="pwd2" class="form-control" onblur="OnBlurPwd()" required>
-                                </div>
-                            </div>
-                            <div class="row mt-3">
-                                <label class="col-lg-3 col-form-label">Name-Surename: </label>
+                                <label class="col-lg-3 col-form-label">ชื่อ-นามสกุล: </label>
                                 <div class="col-lg-9">
                                     <input type="text" name="name" class="form-control" require>
                                 </div>
                             </div>
                             <div class="row mt-3">
-                                <label class="col-lg-3 col-form-label">Gender: </label>
+                                <label class="col-lg-3 col-form-label">เพศ : </label>
                                 <div class="col-lg-9">
                                     <div class="form-check">
                                         <input type="radio" name="gender" value="m" class="form-check-input" require>
-                                        <label class="form-check-label">Male</label>
+                                        <label class="form-check-label">ชาย</label>
                                     </div>
                                     <div class="form-check">
                                         <input type="radio" name="gender" value="f" class="form-check-input" require>
-                                        <label class="form-check-label">Female</label>
+                                        <label class="form-check-label">หญิง</label>
                                     </div>
                                     <div class="form-check">
                                         <input type="radio" name="gender" value="o" class="form-check-input" require>
-                                        <label class="form-check-label">Other</label>
+                                        <label class="form-check-label">อื่นๆ</label>
                                     </div>
                                 </div>
                             </div>
                             <div class="row mt-3">
-                                <label class="col-lg-3 col-form-label">Email: </label>
+                                <label class="col-lg-3 col-form-label">อีเมล : </label>
                                 <div class="col-lg-9">
                                     <input type="email" name="email" class="form-control" require>
                                 </div>
@@ -97,10 +77,10 @@
                             <div class="row mt-3">
                                 <div class="col-lg-12 d-flex justify-content-center">
                                     <button type="submit" class="btn btn-primary btn-sm me-1">
-                                        <i class="bi bi-save"></i> Register
+                                        <i class="bi bi-save"></i> สมัครสมาชิก
                                     </button>
                                     <button type="reset" class="btn btn-danger btn-sm ms-1">
-                                        <i class="bi bi-x-square"></i> Cancel
+                                        <i class="bi bi-x-square"></i> ยกเลิก
                                     </button>
                                 </div>
                             </div>
--- register_save.php
+++ register_save.php
@@ -1,29 +1,19 @@
 <?php
-    session_start();
-    if (isset($_SESSION['id'])) {
-        header("location:index.php");
-        die();
-    }
-
     if (isset($_POST['login'])) {
         $login = $_POST['login'];
-        $passwd = sha1($_POST['pwd']);
+        $passwd = $_POST['pwd'];
         $name = $_POST['name'];
         $gender = $_POST['gender'];
         $email = $_POST['email'];
 
         $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
-        $sql = "SELECT * FROM user WHERE login='$login'";
-        $result = $conn->query($sql);
-        if ($result->rowCount()==1) {
-            $_SESSION['add_login']="error";
-        } else {
-            $sql1 = "INSERT INTO user (login, password, name, gender, email, role) VALUES ('$login', '$passwd', '$name', '$gender', '$email', 'm')";
-            $conn->exec($sql1);
-            $_SESSION['add_login']="success";
-        }
+
+        $sql = "INSERT INTO user (login, password, name, gender, email, role) VALUES ('$login', '$passwd', '$name', '$gender', '$email', 'm')";
+        $conn->exec($sql);
         $conn = null;
-        header("location:register.php");
+        header("location:login.php");
         die();
+    } else { 
+        header("location:index.php");
     }
 ?>
